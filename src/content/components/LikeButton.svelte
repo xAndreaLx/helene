613e29fa06af7cd27c593cbc923cca05aeb53ba4
xAndreaLx <a.lucianoxvx@gmail.com>
2026-03-10 21:42:15 +0100

    update structure + test component

---
+++ src/content/components/LikeButton.svelte
@@ -0,0 +1,12 @@
+<script>
+  let count = 0;
+</script>
+
+<button on:click={() => count++}>
+  ❤️ {count} Likes
+</button>
+
+<style>
+  button { background: #f0f; color: white; border-radius: 8px; }
+</style>
+
